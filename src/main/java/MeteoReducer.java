c32ab0c0ae122344becd9d1aff9256b072660e8f
DiopAmary <amarydiop27@gmail.com>
2021-05-20 23:24:03 +0100

    Mise en place de la classe Reducer

--- src/main/java/MeteoReducer.java
+++ src/main/java/MeteoReducer.java
@@ -1,2 +1,43 @@
-public class MeteoReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class MeteoReducer extends Reducer<Text, MeteoWritable, Text, MeteoWritable> {
+    protected void reduce(Text key, Iterable<MeteoWritable> values, Context context) throws IOException, InterruptedException{
+        double sumWind = 0;
+        double windMin = 0;
+        double windMax = 0;
+        long mesureWindCounter = 0;
+        double temperatureMin = 0;
+        double temperatureMax = 0;
+        long mesureTemperatureCounter = 0;
+        for(MeteoWritable meteoWritable : values){
+            if(meteoWritable.getNombreMesureWind() != 0){
+                mesureWindCounter += meteoWritable.getNombreMesureWind();
+                if(windMin > meteoWritable.getWindMin())
+                    windMin =  meteoWritable.getWindMin();
+                if(windMax < meteoWritable.getWindMax())
+                    windMax = meteoWritable.getWindMax();
+                sumWind += meteoWritable.getWindMoyenne();
+            }
+            if(meteoWritable.getNombreMesureTemperature() != 0){
+                mesureTemperatureCounter += meteoWritable.getNombreMesureTemperature();
+                if(temperatureMin > meteoWritable.getTemperatureMin())
+                    temperatureMin = meteoWritable.getTemperatureMin();
+                if(temperatureMax < meteoWritable.getTemperatureMax())
+                    temperatureMax = meteoWritable.getTemperatureMax();
+            }
+        }
+        MeteoWritable outputValue = new MeteoWritable(
+                mesureTemperatureCounter,
+                mesureWindCounter,
+                temperatureMax,
+                temperatureMin,
+                windMax,
+                windMin,
+                sumWind/mesureWindCounter
+        );
+        context.write(key, outputValue);
+    }
 }
